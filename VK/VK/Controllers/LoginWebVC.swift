b71ebbdfa90f84755a467b30dc27fe816beb2262
KonstantinZavyalov <50868345+KonstantinZavyalov@users.noreply.github.com>
2019-05-30 22:42:18 +0500

    Add work token

---
+++ VK/VK/Controllers/LoginWebVC.swift
@@ -0,0 +1,101 @@
+//
+//  LoginWebVC.swift
+//  VK
+//
+//  Created by Мас on 30/05/2019.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import Alamofire
+
+class LoginWebVC: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView! {
+        didSet {
+            webView.navigationDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "oauth.vk.com"
+        components.path = "/authorize"
+        components.queryItems = [
+            URLQueryItem(name: "client_id", value: "6995632"),
+            URLQueryItem(name: "scope", value: "262158"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "response_type", value: "token"),
+            URLQueryItem(name: "v", value: "5.95")
+        ]
+        
+        let request = URLRequest(url: components.url!)
+        webView.load(request)
+    }
+    
+    static let session: SessionManager = {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 60
+        let session = SessionManager(configuration: config)
+        return session
+    }()
+    
+    func loadGroups(token: String) {
+        let baseUrl = "https://api.vk.com"
+        let path = "/method/groups.get"
+        
+        let params: Parameters = [
+            "access_token": token,
+            "extended": 1,
+            "v": "5.95"
+        ]
+        
+        LoginWebVC.session.request(baseUrl + path, method: .get, parameters: params).responseJSON { response in
+            guard let json = response.value else { return }
+            
+            print(json)
+        }
+    }
+    
+}
+
+extension LoginWebVC: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url,
+            url.path == "/blank.html",
+            let fragment = url.fragment else { decisionHandler(.allow); return }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        print(params)
+        
+        guard let token = params["access_token"],
+            let userIdString = params["user_id"],
+            let userId = Int(userIdString) else {
+                decisionHandler(.allow)
+                return
+        }
+        
+        Session.instance.token = token
+        Session.instance.userId = userId
+        // performSegue(withIdentifier:)
+        
+        LoginWebVC().loadGroups(token: token)
+        
+        decisionHandler(.cancel)
+    }
+}
